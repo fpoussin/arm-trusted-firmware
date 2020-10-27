8896a9869fa66cabb8ff9c3eae427c0ae71d5237
Gabriel Fernandez <gabriel.fernandez@st.com>
2020-10-27 14:09:32 +0100

    stm32mp: use newly introduced minimal clock framework
    Change call of:
    stm32mp_clk_enable() / stm32mp_clk_disable() / stm32mp_clk_get_rate()
    by
    clk_enable() / clk_disable() / clk_get_rate()
    
    Change-Id: I15d2ce57b9499211fa522a1b53eeee9cf584c111
    Signed-off-by: Gabriel Fernandez <gabriel.fernandez@st.com>
    Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/182346
    Reviewed-by: CITOOLS <smet-aci-reviews@lists.codex.cro.st.com>
    Reviewed-by: Yann GAUTIER <yann.gautier@st.com>
    Tested-by: Yann GAUTIER <yann.gautier@st.com>

--- plat/st/stm32mp1/stm32mp1_security.c
+++ plat/st/stm32mp1/stm32mp1_security.c
@@ -10,6 +10,7 @@
 
 #include <common/debug.h>
 #include <drivers/arm/tzc400.h>
+#include <drivers/clk.h>
 #include <drivers/st/stm32mp1_clk.h>
 #include <dt-bindings/clock/stm32mp1-clks.h>
 #include <lib/mmio.h>
@@ -124,8 +125,8 @@ static void init_tzc400(void)
  ******************************************************************************/
 static void early_init_tzc400(void)
 {
-	stm32mp_clk_enable(TZC1);
-	stm32mp_clk_enable(TZC2);
+	clk_enable(TZC1);
+	clk_enable(TZC2);
 
 	/* Region 0 set to cover all DRAM secure at 0xC000_0000 */
 	init_tzc400_begin(TZC_REGION_S_RDWR);
